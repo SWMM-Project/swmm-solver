89f05d86115f63ae6368fd1f73a1cf33bd826a2f
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-11-08 07:32:15 -0500

    Adding Init

---
+++ tools/swmm-output/src/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#
+# Licensed under the terms of the MIT License
+# See LICENSE.txt for details
+# -----------------------------------------------------------------------------
+"""Python Extension for the Output API."""
+
+# Local imports
+from swmm_output import outputapi
+
+VERSION_INFO = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, VERSION_INFO))
+__author__ = 'Michael Tryby (USEPA) - bemcdonnell@gmail.com'
+__copyright__ = 'Public Domain'
+__licence__ = 'MIT'
